7eaa0246b5bf41295409cfe56f7de3e178ff91d7
Gregory Hostin <greghostin@icloud.com>
2026-04-12 12:29:47 +0200

    feat: connect API to database for prediction logging

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import joblib
+from sqlalchemy.orm import Session
 from app.schemas import EmployeeInput, PredictionOutput
 from app.preprocessing import preprocess_input
+from db.database import engine, SessionLocal, Base
+from db.models import PredictionInput, PredictionOutput as PredictionOutputDB
+
+Base.metadata.create_all(engine)
 
 app = FastAPI(
     title="Futurisys ML API",
@@ -13,14 +18,22 @@ model = joblib.load("model/model_xgb.joblib")
 THRESHOLD = 0.545
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/")
 def root():
     return {"message": "Futurisys ML API is running"}
 
 
 @app.post("/predict", response_model=PredictionOutput)
-def predict(employee: EmployeeInput):
-    df = preprocess_input(employee.model_dump()) # converti l'objet Pydantic en dictionnaire python pour le passer au preprocess
+def predict(employee: EmployeeInput, db: Session = Depends(get_db)):
+    df = preprocess_input(employee.model_dump())
     proba = model.predict_proba(df)[:, 1][0]
     prediction = int(proba >= THRESHOLD)
 
@@ -30,6 +43,22 @@ def predict(employee: EmployeeInput):
         risk_level = "modéré"
     else:
         risk_level = "élevé"
+
+    # enregistrer l'input en base
+    db_input = PredictionInput(**employee.model_dump())
+    db.add(db_input)
+    db.commit()
+    db.refresh(db_input)
+
+    # enregistrer l'output en base
+    db_output = PredictionOutputDB(
+        input_id=db_input.id,
+        prediction=prediction,
+        probability=round(float(proba), 3),
+        risk_level=risk_level,
+    )
+    db.add(db_output)
+    db.commit()
 
     return PredictionOutput(
         prediction=prediction,
